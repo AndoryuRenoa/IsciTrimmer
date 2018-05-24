51f5d43042f4cef4562afc4410080fac1ba708bd
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-05-24 12:31:51 -0600

    Create IsciTrimmer.java

---
+++ archive/IsciTrimmer.java
@@ -0,0 +1,27 @@
+
+package iscitrimmer;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class IsciTrimmer {
+
+
+  	 public static void main(String[] args) {
+                 Trimmer trimmer = new Trimmer();
+		 javax.swing.SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                    JFrame frame = new JFrame ("ISCI Trimmer");
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame.getContentPane().add(trimmer);
+                    frame.setVisible(true);
+		    frame.setPreferredSize(new Dimension(300, 110));
+		    frame.setAlwaysOnTop(true); 
+                    frame.pack();
+					 
+			  }
+	
+		 		});
+	 }
+}
